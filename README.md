2fa5916d961073cc62f178e559b818a3db5d2bd7
Renato Gama <renatoargh@gmail.com>
2015-09-03 17:39:33 -0300

    First version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ec2-run-instance-lambda
 AWS Lambda function that runs an EC2 instance with parameters coming from an HTTP request
+
+### Instalation
+
+```bash
+npm install --save ec2-run-instance-lambda
+```
+
+### Usage
+
+```javascript
+'use strict';
+
+var ec2RunInstance = require('ec2-run-instance');
+
+module.exports.handler = ec2RunInstance({
+  region: 'sa-east-1'
+});
+```
+
+### License: MIT
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var Aws = require('aws-sdk'),
+  async = require('async');
+
+function newHandler(ec2parameters) {
+  ec2 = new Aws.EC2(ec2parameters);
+
+  return function handler(event, context) {
+    var instancesCreated = [];
+
+    function runInstances(cb) {
+      ec2.runInstances({
+        ImageId: event.imageId,
+        KeyName: event.keyName,
+        MaxCount: event.maxCount || event.count || 1,
+        MinCount: event.minCount || event.count || 1,
+        Monitoring: {
+          Enabled: event.monitoring || false
+        },
+        InstanceType: event.instanceType || 't2.micro',
+        SecurityGroupIds: event.securityGroupIds,
+        UserData: event.userData || null
+      }, cb);
+    }
+
+    function createTags(reservations, cb) {
+      instancesCreated = reservations.Instances.map(function(reservation) {
+        return reservation.InstanceId;
+      });
+
+      ec2.createTags({
+        Resources: instancesCreated,
+        Tags: event.tags && event.tags.map(function(tag) {
+            return {
+              Key: tag.key,
+              Value: tag.value
+            }
+        })
+      }, cb);
+    }
+
+    async.waterfall([
+      runInstances,
+      createTags
+    ], function(err) {
+      if(err) {
+        return context.fail(err);
+      }
+
+      context.succeed(instancesCreated);
+    });
+  }
+}
+
+module.exports.newHandler = newHandler;
